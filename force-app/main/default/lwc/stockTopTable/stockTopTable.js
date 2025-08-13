801479007e9848e95fda480ea99b9780d20cf1d8
iriteshranjan <ritesh.fps@gmail.com>
2025-08-14 02:33:26 +0530

    added realtime losser gainer

---
+++ force-app/main/default/lwc/stockTopTable/stockTopTable.js
@@ -0,0 +1,45 @@
+import { LightningElement, track } from 'lwc';
+import getTopMarketData from '@salesforce/apex/StockTopWrapper.getTopMarketData';
+
+export default class StockTopTable extends LightningElement {
+    @track marketData;
+    marketType = 'gainers'; // default
+
+    handleGainers() {
+        this.fetchMarketData('gainers', 10);
+    }
+
+    handleLosers() {
+        this.fetchMarketData('losers', 10);
+    }
+    handleClose() {
+    this.marketData = null;
+}
+
+
+    fetchMarketData(type, limit) {
+    this.marketType = type;
+    getTopMarketData({ marketType: type, fetchCount: limit })
+        .then(result => {
+            // Ensure reactive assignment
+            let nseData = result.nse.data.map(item => ({
+                ...item,
+                percentChange: type === 'gainers' ? item.gain : item.loss
+            }));
+            let bseData = result.bse.data.map(item => ({
+                ...item,
+                percentChange: type === 'gainers' ? item.gain : item.loss
+            }));
+
+            // Assign back so LWC can see updates
+            result.nse.data = nseData;
+            result.bse.data = bseData;
+
+            this.marketData = result;
+        })
+        .catch(error => {
+            console.error('Error fetching market data:', error);
+        });
+}
+
+}
