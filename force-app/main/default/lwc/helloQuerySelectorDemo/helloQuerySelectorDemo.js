d15e508a886d722fd0218b9cf46d23c6e27e63eb
iriteshranjan <ritesh.fps@gmail.com>
2025-08-21 09:13:19 +0530

    lightning:dom='manual'

---
+++ force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloQuerySelectorDemo extends LightningElement {
+
+    userNames=["a","b","c","d"];
+    submitHandler(event)
+    {
+
+
+          //lwc:manual dom
+        const childElement=this.template.querySelector('.child');
+        childElement.innerHTML='<p> Hey I am child</p>'
+
+
+       const elem= this.template.querySelector('h1');
+       elem.style.border="1px solid red";
+       console.log(elem)
+       console.log(elem.innerText);
+
+
+       const userElements=this.template.querySelectorAll('.name');
+       Array.from(userElements).forEach(item=>{
+        console.log(item.innerText)
+        item.setAttribute("title",item.innerText)
+
+      
+       })
+
+    }
+}
