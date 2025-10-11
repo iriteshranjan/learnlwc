e9a57b5c7916999501ec51b147470a5568cd4b00
iriteshranjan <ritesh.fps@gmail.com>
2025-10-12 00:25:04 +0530

    restart

--- force-app/main/default/lwc/indexesLiveTest/indexesLiveTest.js
+++ force-app/main/default/lwc/indexesLiveTest/indexesLiveTest.js
@@ -14,6 +14,7 @@ export default class IndexesLiveTest extends LightningElement {
 
     disconnectedCallback() {
         clearInterval(this.refreshInterval);
+        //restart
     }
 
     async fetchIndexes() {
