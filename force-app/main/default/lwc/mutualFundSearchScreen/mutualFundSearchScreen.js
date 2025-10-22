4790be7a5a37ab635d0bfdc64835aa1b5d0c6496
iriteshranjan <ritesh.fps@gmail.com>
2025-10-22 06:48:03 +0530

    mutual

--- force-app/main/default/lwc/mutualFundSearchScreen/mutualFundSearchScreen.js
+++ force-app/main/default/lwc/mutualFundSearchScreen/mutualFundSearchScreen.js
@@ -1,4 +1,27 @@
-import { LightningElement } from 'lwc';
-import 'mutualFundSearch' from '@salesforce/apex/'
+import { LightningElement,track, wire } from 'lwc';
+import mutualFundSearch from '@salesforce/apex/mutualFundLwc.selectmutualFund';
 
-export default class MutualFundSearchScreen extends LightningElement {}
+export default class MutualFundSearchScreen extends LightningElement {
+    @track mutualfundsData
+    searchValue='';
+    allFunds=[]
+ 
+    searchHandler(event)
+    {
+        this.searchValue=event.target.value;
+        if(this.searchValue && this.searchValue.length>2)
+        {
+           mutualFundSearch({fundName:this.searchValue}).then(
+            result=>{
+                this.allFunds=result && result.length>0 ?
+                result:[{Id:'null',direct_scheme_name__c:'No Results found'}];
+            }
+           );
+                
+            
+        }
+        
+       
+
+   }
+}
